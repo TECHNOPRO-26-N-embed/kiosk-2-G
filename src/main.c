0d63ac4228dd4633ecc95c451692d189d343f1af
282408132 <282408132>
2026-05-19 10:41:52 +0900

    yano

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ void drinksPrice(Drink drinks[], int choice, int *money){
     int j;
     j= *money  -   drinks[choice-1].price ;
             if(j>0){
-            printf("お釣りは%d円です\n",j);
+            printf("残り残金は%d円です\n",j);
             *money -= drinks[choice-1].price;
             }else{
                 printf("お釣りはありません\n");
