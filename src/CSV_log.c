f97ac13f3102bc38860ee08c4f49e9b03cae35e6
Yoji-Sato-DS <282408232+Yoji-Sato-DS@users.noreply.github.com>
2026-05-20 13:44:57 +0900

    CSV

--- src/CSV_log.c
+++ src/CSV_log.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
 /* F05 売上データ保存 */
 void saveSalesData(int id, char name[], int price, int stock) {
@@ -11,14 +10,7 @@ void saveSalesData(int id, char name[], int price, int stock) {
         return;
     }
 
-    // 日時取得
-    time_t t = time(NULL);
-    struct tm *tm_info = localtime(&t);
-    char datetime[20];
-    strftime(datetime, sizeof(datetime), "%Y-%m-%d %H:%M:%S", tm_info);
-
-    // 日時を先頭にしてCSVに出力
-    fprintf(fp, "%s,%d,%s,%d,%d\n", datetime, id, name, price, stock);
+    fprintf(fp, "%d,%s,%d,%d\n", id, name, price, stock);
     fclose(fp);
 
     printf("売上データを保存しました。\n");
